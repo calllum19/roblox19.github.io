af160bc5790eafe35599bcf141f0eb2e08c5f810
calllum19 <46068466+calllum19@users.noreply.github.com>
2018-12-31 15:35:27 +0700

    Add files via upload

---
+++ RobloxJS.js
@@ -0,0 +1,12 @@
+function AlertIt() {
+var answer = confirm ("You are going to be redirected to ROBLOX. Continue?")
+if (answer)
+window.location="https://www.roblox.com/";
+}
+function rightCode() {
+  var myInput = document.getElementById('password')
+  if (myInput.value == "s3cret") {
+      alert('@#!#!@#!$!@#!@$!@#!@#*%%');
+      window.location = "https://static.makeuseof.com/wp-content/uploads/2009/12/scary6.jpg"
+  }
+}
